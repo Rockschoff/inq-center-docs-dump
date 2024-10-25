db30d115c4df812293b62fec2163e49f1c28d13c
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-10-25 07:34:47 -0700

    initial commit

--- app.py
+++ app.py
@@ -91,4 +91,4 @@ def delete_files():
         return jsonify({'message': str(e)}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5544)
+    app.run(debug=True, host='0.0.0.0', port=5554)
